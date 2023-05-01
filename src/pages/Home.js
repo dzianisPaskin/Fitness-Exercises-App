391e8e02f7f7a7bb10e09156a9a2ef242691dbc4
Denis Paskin <paskin.denis.00@gmail.com>
2023-05-01 09:34:00 +0200

    implemented the SearchExercises, HorizontalScrollBar and BodyPart components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,29 @@
-import React, {useState} from 'react'
-import { Box } from '@mui/material'
-import { Search } from '@mui/icons-material'
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
-import HeroBanner from '../components/HeroBanner'
-import SearchExercises from '../components/SearchExercises'
-import Exercises from '../components/Exercises'
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all');
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
-      <HeroBanner/>
-      <SearchExercises/>
-      <Exercises/>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
