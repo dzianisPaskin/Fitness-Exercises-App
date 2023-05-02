5b4fead25f3baa3c91a6d5ade94bf290fd9bd3bc
Denis Paskin <paskin.denis.00@gmail.com>
2023-05-02 11:12:52 +0200

    modified the fetchData and ExerciseDetail components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,35 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
-  const [exerciseDetail, setExerciseDetail] = useState({})
-  const {id} = useParams()
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([])
+  const { id } = useParams();
 
-  useEffect(() =>{ 
-    const fetchExercisesData =  async () => {
-      const exericeDbUrl = 'https://exercisedb.p.rapidapi.com'
-      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exericeDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl =
+        'https://youtube-search-and-download.p.rapidapi.com';
 
-      const exerciseDetailData = await fetchData(`${exericeDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+      const exerciseDetailData = await fetchData(
+        `${exericeDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
 
-      setExerciseDetail(exerciseDetailData)
-    }
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData)
+    };
 
-    fetchExercisesData()
-  }, [id])
-
+    fetchExercisesData();
+  }, [id]);
 
   return (
     <Box>
-      <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos />
-      <SimilarExercises/>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos}/>
+      <SimilarExercises />
     </Box>
   );
 };
